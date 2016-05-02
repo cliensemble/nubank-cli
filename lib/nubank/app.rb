7cee74700b75d7949941e89d04ba45089dfd0dee
Rodrigo Muniz <muniz95@bsd.com.br>
2016-05-02 02:09:18 +0000

    First commit

--- lib/nubank/app.rb
+++ lib/nubank/app.rb
@@ -1,12 +1,37 @@
+require 'curb'
 require 'thor'
 
 module NubankCli
-    class App < Thor
-        
-        desc 'teste', 'teste'
-        def teste
-            puts 'teste'
-        end
-        
+  class App < Thor
+  
+    package_name 'tpl'
+    
+    desc "setup", "Create config and edit with $EDITOR"
+    def setup
+      Configuration.save
+      if !ENV['EDITOR'].to_s.empty? && !ENV['EDITOR'].nil?
+        exec "$EDITOR #{ENV['HOME']}/.nubankrc"
+      else
+        puts "$EDITOR is not set. Please type your editor:"
+        editor = STDIN.gets.chomp
+        exec "#{editor} #{ENV['HOME']}/.nubankrc"
+      end
     end
+    
+    desc 'teste', 'teste'
+    def teste
+      puts 'teste'
+    end
+    
+    private
+    def login
+      puts "Usuário:"
+      @user = STDIN.gets.chomp
+      puts "Senha:"
+      @pass = STDIN.gets.chomp
+      
+      
+    end
+  
+  end
 end
